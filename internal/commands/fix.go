d266f5eaa01b8faaca2250814bac0ab5d70b2b4e
Cyril Levis <git@levis.name>
2022-04-25 08:05:22 +0200

    fix: check RemoveBridge first

--- internal/commands/fix.go
+++ internal/commands/fix.go
@@ -13,24 +13,25 @@ import (
 type FixCommand struct {
 	CreateBridge bool `short:"c" name:"create-bridge" help:"Create bridge"`
 	RemoveBridge bool `short:"r" name:"remove-bridge" help:"Remove bridge"`
-	Labels       bool `short:"l" name:"labels" help:"Fix selinux labels"`
+	Labels       bool `short:"l" name:"labels"        help:"Fix SElinux labels"`
 }
 
 // Run start the fix command
 func (c *FixCommand) Run(s *options.Options) error {
 	command := exec.Command("bash")
 
-	if c.CreateBridge || (!c.CreateBridge && !c.Labels) {
-		command.Stdin = strings.NewReader(scripts.CreateBridgeScript)
+	if c.RemoveBridge {
+		command.Stdin = strings.NewReader(scripts.RemoveBridgeScript)
 		b, err := command.Output()
 		if err != nil {
 			fmt.Println(err)
 		}
 		fmt.Println(string(b))
+		return nil
 	}
 
-	if c.Labels || (!c.CreateBridge && !c.Labels) {
-		command.Stdin = strings.NewReader(scripts.FixLabelsScript)
+	if c.CreateBridge || (!c.CreateBridge && !c.Labels) {
+		command.Stdin = strings.NewReader(scripts.CreateBridgeScript)
 		b, err := command.Output()
 		if err != nil {
 			fmt.Println(err)
